e8f8e3538daa8b9abb7b5b3893be67790a7461f3
lvingonzales <lvingonzales15@gmail.com>
2024-11-23 11:20:40 -0500

    Finish Canvas and basic canvas function

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 const CONTAINER_LENGTH = 800;
+const container = document.querySelector("#container");
 
-console.log (CONTAINER_LENGTH);
+
+let canvasSize = 20;
+let canvasArea;
+let canvasLength;
+let canvasSquares = [];
+
+function drawCanvas() {
+    canvasArea = canvasSize * canvasSize;
+    canvasLength = CONTAINER_LENGTH / canvasSize;
+
+    for (let i = 1; i <= canvasArea; i++){
+        let newSquare = document.createElement("div");
+            newSquare.setAttribute("class", "canvas");
+            newSquare.style.height = canvasLength + 'px';
+            newSquare.style.width = canvasLength + 'px';
+            newSquare.addEventListener("mouseover", () => {
+                newSquare.style.backgroundColor = 'blue';
+            });
+            container.appendChild(newSquare);
+            canvasSquares.push(newSquare);
+    }
+}
+
+drawCanvas();
+
